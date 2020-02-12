63d55e4448f28c9d50d25818c1c596009d2b2175
Sophie McGarity <sophiemcgarity7@gmail.com>
2020-02-11 16:07:31 -0800

    mongoose part 2

--- index.js
+++ index.js
@@ -12,12 +12,10 @@ connect.then(() => {
 
     console.log('Connected to the server');
 
-    const newCampsite = new Campsite({
+    Campsite.create({
         name: 'React Lake Campground',
         description: 'test'
-    });
-
-    newCampsite.save()
+    })
     .then(campsite => {
         console.log(campsite);
         return Campsite.find();
